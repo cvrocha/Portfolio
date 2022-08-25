a2520c434b3cdcfc74ee5ef9d8cc36f5cdcf8135
cvrocha <cvrochaa13@gmail.com>
2022-08-25 11:04:25 -0300

    ==TESTE DE DARKMODE 3

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,18 +1,4 @@
-function changeToDarkMode(settings) {
+function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
-   if (settings && settings.smooth_transition) {
-   element.classList.toggle("smooth-transition");
-   }
 }
-
-const modeSwitch = body.querySelector(".toggle-switch");
-modeSwitch.addEventListener("click", () => {
-   body.classList.toggle("dark");
-
-   if (body.classList.contains("dark")) {
-      modeText.innerText = "Light mode";
-   } else {
-      modeText.innerText = "Dark mode";
-   }
-});
